e723a6e04a2c42375a61a48b4503355672c31925
byteit101 <byteit101>
2011-01-30 20:30:53 -0500

    Adding team number retrival

--- plugins/builder/src/ucpp/Activator.java
+++ plugins/builder/src/ucpp/Activator.java
@@ -1,5 +1,14 @@
 package ucpp;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.core.resources.IProject;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -56,6 +65,60 @@ public class Activator extends AbstractUIPlugin
 	public static Activator getDefault()
 	{
 		return plugin;
+	}
+
+	public static int GetTeamNumber(IProject project)
+	{
+		// export DEPLOY_IP=10.4.51.2
+		String f = GetFileContents(project.getLocation().toString() + "/.ucpp");
+		Pattern p = Pattern.compile("export DEPLOY_IP=10\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.2");
+		Matcher m = p.matcher(f);
+		m.find();
+		int r = Integer.valueOf(m.group(1))*100;
+		r += Integer.valueOf(m.group(2));
+		return r;
+	}
+
+	public static String GetFileContents(String fileName)
+	{
+		File file = new File(fileName);
+		StringBuffer contents = new StringBuffer();
+		BufferedReader reader = null;
+
+		try
+		{
+			reader = new BufferedReader(new FileReader(file));
+			String text = null;
+			
+			while ((text = reader.readLine()) != null)
+			{
+				contents.append(text).append(System.getProperty("line.separator"));
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				if (reader != null)
+				{
+					reader.close();
+				}
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+
+		return contents.toString();
 	}
 
 }
--- plugins/builder/src/ucpp/builder/DeployToRobotAction.java
+++ plugins/builder/src/ucpp/builder/DeployToRobotAction.java
@@ -76,37 +76,14 @@ public class DeployToRobotAction implements IObjectActionDelegate
 	{
 		try
 		{
-			if (OSValidator.isUnix() || OSValidator.isMac())
-			{
-				Runtime rt = Runtime.getRuntime();
-				Process pr = rt.exec(new String[] { "make", "deploy" }, null, project.getLocation().toFile());
-
-				BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-
-				String line = null;
-
-				while ((line = input.readLine()) != null)
-				{
-					System.out.println(line);
-				}
-
-				int exitVal = pr.waitFor();
-				System.out.println("Exited with error code " + exitVal);
-			}
-			else if (OSValidator.isWindows())
-			{
-				int team = 451;//TODO: remove this hard code
-				FileTransferClient ftp = new FileTransferClient();
-				ftp.setRemoteHost("10."+(team/100)+"."+(team%100)+".2");
-				ftp.connect();
-				//(project)/PPC603gnu/projectname/Debug/?
-				ftp.uploadFile(project.getLocation().toString()+"/PPC603gnu/"+project.getName()+"/Debug/"+project.getName()+".out", "/ni-rt/system/FRC_UserProgram.out");
-				ftp.disconnect();
-			}
-			else
-			{
-				System.out.println("UNSUPPORTED SYSTEM!");
-			}
+			int team = ucpp.Activator.GetTeamNumber(project);// TODO: remove this hard code
+			System.out.println(team);
+			FileTransferClient ftp = new FileTransferClient();
+			ftp.setRemoteHost("10." + (team / 100) + "." + (team % 100) + ".2");
+			ftp.connect();
+			// (project)/PPC603gnu/projectname/Debug/projectname.out
+			ftp.uploadFile(project.getLocation().toString() + "/PPC603gnu/" + project.getName() + "/Debug/" + project.getName() + ".out", "/ni-rt/system/FRC_UserProgram.out");
+			ftp.disconnect();
 		}
 		catch (Exception e)
 		{
