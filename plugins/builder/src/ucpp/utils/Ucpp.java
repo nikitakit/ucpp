f285407e5d79d3fd98c27fe1038696392a8bc8ba
byteit101 <byteit101@nospam>
2011-02-24 14:51:03 -0500

    fixing ucpp call

--- plugins/builder/src/ucpp/utils/Ucpp.java
+++ plugins/builder/src/ucpp/utils/Ucpp.java
@@ -30,12 +30,12 @@ public class Ucpp
 
 	public static ReturnValue build(IProject project) throws Exception
 	{
-		return exe("make", project);
+		return exec("make", project);
 	}
 
 	public static ReturnValue clean(IProject project) throws Exception
 	{
-		return exe("make clean", project);
+		return exec("make clean", project);
 	}
 
 	private static ReturnValue exe(String command, IProject project) throws Exception
@@ -55,6 +55,45 @@ public class Ucpp
 		else if (OSValidator.isWindows())
 		{
 			pr = rt.exec("C:\\Program Files\\Git\\bin\\bash.exe --login -i -c 'cd \"" + project.getLocation().toFile() + "\"; ucpp -s " + command + "'", null, project.getLocation().toFile());
+		}
+		else
+		{
+			System.out.println("UNSUPPORTED SYSTEM!");
+			throw new Exception("UNSUPPORTED SYSTEM!");
+		}
+
+		BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+		String line = null;
+		String lines = "";
+
+		while ((line = input.readLine()) != null)
+		{
+			System.out.println(line);
+			lines += line + ReturnValue.lineSeparator;
+		}
+
+		int exitVal = pr.waitFor();
+		if (exitVal != 0)
+			System.out.println("Exited with error code " + exitVal);
+
+		return new ReturnValue(exitVal, lines);
+	}
+	
+	private static ReturnValue exec(String command, IProject project) throws Exception
+	{
+		System.out.println("executing '" + command + "'");
+
+		Runtime rt = Runtime.getRuntime();
+		Process pr = null;
+		
+		if (OSValidator.isUnix() || OSValidator.isMac())
+		{
+			String path = System.getenv("PATH");
+			pr = rt.exec(command, new String[] { "PATH=" + path }, project.getLocation().toFile());
+		}
+		else if (OSValidator.isWindows())
+		{
+			pr = rt.exec("C:\\Program Files\\Git\\bin\\bash.exe --login -i -c 'cd \"" + project.getLocation().toFile() + "\"; " + command + "'", null, project.getLocation().toFile());
 		}
 		else
 		{
